db6ebd45ab34c7c870757051b159064bb8bf6e1e
Antonio Fasolato <antonio.fasolato@gmail.com>
2023-04-06 07:31:47 +0200

    Mock Datatable

--- lib/scenes/table_scene.dart
+++ lib/scenes/table_scene.dart
@@ -7,14 +7,42 @@ class TableScene extends StatelessWidget {
 
   final String title;
 
+  List<DataColumn> _createColumns() {
+    return [
+      DataColumn(label: Text('ID')),
+      DataColumn(label: Text('Book')),
+      DataColumn(label: Text('Author'))
+    ];
+  }
+
+  List<DataRow> _createRows() {
+    return [
+      DataRow(cells: [
+        DataCell(Text('#100')),
+        DataCell(Text('Flutter Basics')),
+        DataCell(Text('David John'))
+      ]),
+      DataRow(cells: [
+        DataCell(Text('#101')),
+        DataCell(Text('Dart Internals')),
+        DataCell(Text('Alex Wick'))
+      ])
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Header(
         title: title,
       ),
-      body: const Center(
-        child: Text("Table"),
+      body: ListView(
+        children: [
+          DataTable(
+            columns: _createColumns(),
+            rows: _createRows(),
+          )
+        ],
       ),
       bottomNavigationBar: BottomNavigation(
         title: title,
