e67d28e2967060ff7d071191460804de2a7ce758
Antonio Fasolato <antonio.fasolato@gmail.com>
2023-04-07 18:00:21 +0200

    Transformed into stateful widget

--- lib/scenes/projects_scene.dart
+++ lib/scenes/projects_scene.dart
@@ -4,55 +4,42 @@ import '../components/bottom_navigation.dart';
 import '../components/project_card.dart';
 import '../model/project_dao.dart';
 
-class ProjectsScene extends StatelessWidget {
+class ProjectsScene extends StatefulWidget {
   const ProjectsScene({super.key, required this.title});
 
   final String title;
-  final ProjectDao project = const ProjectDao(
-      code: "TORTPR001", description: "Analisi per Studio Torta");
 
-  List<ProjectDao> _createProjects() {
-    return [
-      const ProjectDao(code: "NETPR035", description: "Swabmonitor"),
-      const ProjectDao(code: "OMIPR154", description: "Wiki Omicron"),
-      const ProjectDao(code: "TORPR008", description: "Torta fase 2"),
-      const ProjectDao(
-          code: "OMIPR096",
-          description: "Tavolo architetture - review infrastruttura"),
-      const ProjectDao(code: "ITCPR009", description: "Assistenza Italiacamp"),
-      const ProjectDao(code: "OMIPR154", description: "Gestione CV"),
-      const ProjectDao(code: "OMIPR123", description: "Stageit"),
-      const ProjectDao(code: "PDCPR001", description: "Crypto"),
-      const ProjectDao(code: "OMIPR154", description: "Fermo attivita'"),
-      const ProjectDao(
-          code: "OMIPR154", description: "Netgenomics backup ambiente"),
-      const ProjectDao(code: "OMIPR154", description: "ChatGPT"),
-      const ProjectDao(code: "OMIPR124", description: "ESG - architettura"),
-      const ProjectDao(code: "OMIPR125", description: "PMS"),
-      const ProjectDao(code: "OMIPR126", description: "Edison DpassWeb"),
-      const ProjectDao(
-          code: "OMIPR127", description: "Italiacamp stima nuovo flusso"),
-      const ProjectDao(code: "RECRUHO", description: "Colloqui"),
-    ];
-  }
+  @override
+  State createState() => _ProjectSceneState();
+}
+
+class _ProjectSceneState extends State<ProjectsScene> {
+  List<ProjectDao> _projects = [];
 
   List<Widget> _createCards() {
-    return _createProjects().map((p) => ProjectCard(project: p)).toList();
+    return _projects.map((p) => ProjectCard(project: p)).toList();
+  }
+
+  void _addProject() {
+    setState(() {
+      _projects.add(
+        const ProjectDao(code: "NETPR035", description: "Swabmonitor"),
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Header(title: title),
+      appBar: Header(title: widget.title),
       body: Center(
         child: ListView(
           children: _createCards(),
         ),
       ),
-      bottomNavigationBar: BottomNavigation(title: title),
+      bottomNavigationBar: BottomNavigation(title: widget.title),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => ScaffoldMessenger.of(context)
-            .showSnackBar(const SnackBar(content: Text("Action"))),
+        onPressed: _addProject,
         tooltip: 'Increment',
         child: const Icon(Icons.add_task),
       ),
