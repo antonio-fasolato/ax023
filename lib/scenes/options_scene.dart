dde7e2fffd3bc51053bf10900e83ff3f8bfa8003
Antonio Fasolato <antonio.fasolato@gmail.com>
2023-04-12 16:32:13 +0200

    File chooser

--- lib/scenes/options_scene.dart
+++ lib/scenes/options_scene.dart
@@ -1,10 +1,14 @@
 import 'package:AX023/components/bottom_navigation.dart';
 import 'package:AX023/components/header.dart';
+import 'package:AX023/constants.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 class OptionsScene extends StatelessWidget {
   final String title;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final Logger logger = Logger();
 
   OptionsScene({super.key, required this.title});
 
@@ -18,16 +22,31 @@ class OptionsScene extends StatelessWidget {
           padding: const EdgeInsets.all(40),
           child: Column(
             children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                    labelText: "Cartella del database",
-                    icon: Icon(Icons.folder)),
-                validator: (value) {
-                  if (value?.isEmpty ?? false) {
-                    return "Inserire una cartella esistente";
-                  }
-                  return null;
-                },
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: "Cartella del database",
+                          icon: Icon(Icons.folder)),
+                      validator: (value) {
+                        if (value?.isEmpty ?? false) {
+                          return "Inserire una cartella esistente";
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () async {
+                        String? selectedDirectory = await FilePicker.platform
+                            .getDirectoryPath(
+                                dialogTitle:
+                                    "Selezionare la cartella di salvataggio del database");
+                        logger.d(selectedDirectory);
+                      },
+                      icon: const Icon(Icons.more_horiz))
+                ],
               ),
             ],
           ),
