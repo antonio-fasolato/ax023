8e1c23099bb94a2eccd91a535284f0715f1082d6
Antonio Fasolato <antonio.fasolato@gmail.com>
2023-04-12 15:53:23 +0200

    Added a form

--- lib/scenes/options_scene.dart
+++ lib/scenes/options_scene.dart
@@ -4,14 +4,35 @@ import 'package:flutter/material.dart';
 
 class OptionsScene extends StatelessWidget {
   final String title;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  const OptionsScene({super.key, required this.title});
+  OptionsScene({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Header(title: title),
-      body: Text("Options"),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(40),
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Cartella del database",
+                    icon: Icon(Icons.folder)),
+                validator: (value) {
+                  if (value?.isEmpty ?? false) {
+                    return "Inserire una cartella esistente";
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
       bottomNavigationBar: BottomNavigation(
         title: title,
       ),
